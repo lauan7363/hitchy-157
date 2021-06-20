9e91ed0d7f377872d4551729885881dc1d49a8f4
Lauan <78829707+lauan7363@users.noreply.github.com>
2021-06-20 16:44:13 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -44,15 +44,15 @@ const titlemenumsgimg = 'BEM-VINDO'
 //Título no menu da imagem quando welcome esta ativado
 //Obs: não coloque o texto longo para a imagem não sumir
 
-const backgroundmenuimg = `https://i.imgur.com/tVKFNFk.png`
+const backgroundmenuimg = `https://i.imgur.com/8oLf9CS.png`
 //Link direto da imagem de fundo TAMANHO RECOMENDADO: 1280x720
 //Obs: não coloque o texto longo para a imagem não sumir
 
-const backgroundwelcomeimg = `https://i.imgur.com/tVKFNFk.png`
+const backgroundwelcomeimg = `https://i.imgur.com/qGqiUSs.png`
 //Link direto da imagem de fundo da boas-vindas TAMANHO RECOMENDADO: 1280x720
 //Obs: não coloque o texto longo para a imagem não sumir
 
-const backgroundbyeimg = `https://i.imgur.com/2TCj9ri.jpg`
+const backgroundbyeimg = `https://i.imgur.com/qGqiUSs.jpg`
 //Link direto da imagem de fundo das despedidas TAMANHO RECOMENDADO: 1280x720
 //Obs: não coloque o texto longo para a imagem não sumir
 
